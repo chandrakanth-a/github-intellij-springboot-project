e7f77cc28a82b114e3679afa7ac7c2855e57b545
Chandrakanth Reddy <chandrakanthreddy7719@gmail.com>
2025-08-17 16:19:20 +0530

    init commit

--- src/main/java/com/example/apigw/PublicApiGatewayApplication.java
+++ src/main/java/com/example/apigw/PublicApiGatewayApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PublicApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(PublicApiGatewayApplication.class, args);
-        //New code change in GitLab
-        //New code changes in Intellij
+
     }
 }
